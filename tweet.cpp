4a121e647b945a149ba7b1d9094deea24477cf2d
Charlie <charlie@debianBox.rbh.rockynet.wireless.unca>
2012-12-07 04:20:47 -0500

    tweets are complete, file tweets.txt displays all words, and lexical diversity at the end.

--- tweet.cpp
+++ tweet.cpp
@@ -13,14 +13,15 @@ using std::endl;
 
 
 int main() {
-
+ofstream tweetFile;
 
 string tweetLine;
 map <string,int> tweetMap; 
 
 map<string,int>::iterator it;
 string line,subLine;
-
+int uniqueCount;
+double lex;
 ifstream tweets ("tweets"); /* ifstream for input, read tweets file*/
 if(tweets.is_open()){
     
@@ -42,20 +43,31 @@ if(tweets.is_open()){
     tweets.close();
 
 }
-
+	  tweetFile.open("tweets.txt");
     
     for(it = tweetMap.begin(); it != tweetMap.end(); it++){
 	  
-		 if(it->second <= 5000 ){ 
+	   if(it->second <= 5000 ){ 
 		 
-		 cout << it->first<< "Appears:  " << it->second << endl;
-
-
-		 }
+		// cout << it->first<< "Appears:  " << it->second << endl;
+		
+		tweetFile << it->first << "  Appears:  " << it->second << endl;
+     
+		uniqueCount++;
+          }
+		 
 		 
 
-    }
-    
+    } 
+	  lex = uniqueCount / tweetMap.size();
+	  
+ tweetFile << "The number of unique words are " << uniqueCount << "."<< endl;  
+	  
+     tweetFile << "Total number of words are " << tweetMap.size() << "."<< endl;  
+
+	  tweetFile << "Lexical Diversity is equal to " << lex << "." << endl;  
+
+	  tweetFile.close();
 
 
 
